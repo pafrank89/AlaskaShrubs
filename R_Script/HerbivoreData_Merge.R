10fee62d819495b7d2bbff6289ec552ebf29e9d8
Peter <pafrank89@gmail.comgit config --global user.email pafrank89@gmail.com>
2019-11-23 16:12:16 +0100

    Updates

--- R_Script/HerbivoreData_Merge.R
+++ R_Script/HerbivoreData_Merge.R
@@ -3,20 +3,20 @@
 # peterfr@stud.ntnu.no
 # 2019-11-21
 
-install.packages("dplR")
-install.packages("reshape2")
-install.packages('plyr')
+# Melt moose density data  
+MooseDensity_Data<-melt(MooseDensity_Data, id="GMU", na.rm = TRUE)
 
-library(dplR)
-library(dplyr)
-library(reshape2)
-library(plyr)
+#Rename the columns which were given generic names in the melt
+colnames(MooseDensity_Data)[colnames(MooseDensity_Data)=="variable"] <- "Year"
+colnames(MooseDensity_Data)[colnames(MooseDensity_Data)=="value"] <- "MooseDensity"
 
-# Processes herbivore data 
+#Add a new column combining GMU and Year on which to join to the larger dataset
+MooseDensity_Data$GMU_year <- do.call(paste, c(MooseDensity_Data[c("GMU", "Year")], sep = "_"))
 
+#Deletes the Year and GMU columns which are now merged, so that they don't show up in the big dataset
+MooseDensity_Data[1] = NULL
+MooseDensity_Data[1] = NULL
 
-#### 1. HERBIVORE DATA ####
+#Reorders the columns 
+MooseDensity_Data = MooseDensity_Data[,c(2,1)]
 
-colnames(Herbivore_Data)[colnames(Herbivore_Data)=="X1"] <- "GMU"
-
-herbivore_melt<-melt(Herbivore_Data, id="GMU", na.rm = TRUE)
---
+++ R_Script/ShrubData_Model.R
@@ -0,0 +1,6 @@
+# Modeling the effects of Climate and Herbivory on Shrub Growth
+# Peter Frank 
+# peterfr@stud.ntnu.no
+# 2019-11-23
+
+
