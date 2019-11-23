70327ed0f6333882a00f36b914963be14f219697
Peter <pafrank89@gmail.comgit config --global user.email pafrank89@gmail.com>
2019-11-23 16:42:03 +0100

    Update

--- R_Script/ShrubData_Model.R
+++ R_Script/ShrubData_Model.R
@@ -3,4 +3,25 @@
 # peterfr@stud.ntnu.no
 # 2019-11-23
 
+install.packages("dplR")           
+install.packages("reshape2")        
+install.packages("dplyr")           
+install.packages("lme4")           
+install.packages("nlme") 
 
+library(dplR)
+library(reshape2)
+library(dplyr)
+library(lme4)
+library(nlme)
+
+# Develop mixed effects models 
+  
+  #Model Structure
+    #lme(fixed, data, random, correlation, weights, subset, method,
+    # na.action, control, contrasts = NULL, keep.data = TRUE)
+
+model_BAI <- lme(BAI ~ summ.rain + MooseDensity, data = ShrubCCH_Data, random = ~ 1,
+                 na.action=na.pass)
+
+summary(model_BAI)
