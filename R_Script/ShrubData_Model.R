69a58988e954a0ba17674ac5b2c028f3896896c2
Peter <pafrank89@gmail.comgit config --global user.email pafrank89@gmail.com>
2019-11-25 22:33:00 +0100

    Update

--- R_Script/ShrubData_Model.R
+++ R_Script/ShrubData_Model.R
@@ -23,23 +23,80 @@ library(PerformanceAnalytics)
 # Check for correlations between variables
 ShrubCCH_DataX = subset (ShrubCCH_Data, select = c("RingWidth", "BAI", "Age", "Section", "Species", "Elevation", "Slope", "Y_Cord", "CanopyCover", 
                                                    "summ.temp", "temp", "summ.min", "summ.max", "wint.rain", "summ.rain", "frost", "wet", "pet",
-                                                   "HareIndex", "PropMoose", "PropHare", "PropPtarmagin", "BinaryBrowse", "MooseDensity"))
+                                                   "HareIndex", "MooseDensity", "PropMoose", "PropHare"))
 
 ShrubCCH_DataX = subset (ShrubCCH_Data, select = c("RingWidth", "BAI", "Age", "Elevation", "Slope", "Y_Cord", "CanopyCover", 
                                                    "summ.temp", "temp", "wint.rain", "summ.rain", "frost", "wet",
-                                                   "PropMoose", "PropHare", "PropPtarmagin", "BinaryBrowse"))
-           
+                                                   "PropMoose", "PropHare", "BinaryBrowse"))
+
+#Climate and BAI correlations
+ShrubCCH_DataX = subset (ShrubCCH_Data, select = c("RingWidth", "BAI",  
+                                                   "summ.temp", "temp", "summ.min", "summ.max", "wint.rain", "summ.rain", "frost", "wet", "pet"))
+                 
+#Herbivore and BAI correlations                                  
+#Not enough observations to include ptarmagin browsing 
+ShrubCCH_DataX = subset (ShrubCCH_Data, select = c("RingWidth", "BAI", "Age",
+                                                   "HareIndex", "MooseDensity", "PropMoose", "PropHare"))
+#Climate and Herbivory correlations
+ShrubCCH_DataX = subset (ShrubCCH_Data, select = c("summ.temp", "temp", "summ.min", "summ.max", "wint.rain", "summ.rain", "frost", "wet", "pet", 
+                                                   "HareIndex", "MooseDensity"))
+#Environmental Covariates and BAI
+ShrubCCH_DataX = subset (ShrubCCH_Data, select = c("RingWidth", "BAI", "Age",  
+                                                   "Elevation", "Slope", "Y_Cord", "CanopyCover", "DistToRoad"))
+
+
+#Creates a correlation matrix using the variables specified above
 chart.Correlation(ShrubCCH_DataX, histogram = TRUE, method = c("pearson"))
+
+#Plot BAI as a function of soil texture
+plot(BAI ~ factor(SoilText), data = ShrubCCH_Data, notch = TRUE,varwidth = TRUE,
+     col = "white", border = "black", horizontal = TRUE, las = 2,
+     pch = 1, ylab = "Basal Area Increment", xlab = "")
+
+#plot BAI as a function of soil moisture
+plot(BAI ~ factor(SoilMoist), data = ShrubCCH_Data, notch = TRUE,varwidth = TRUE,
+     col = "white", border = "black", horizontal = TRUE, las = 2,
+     pch = 1, ylab = "Basal Area Increment", xlab = "")
+
+#Plot BAI as a function of observed vegetation cover 
+plot(BAI ~ factor(VegCoverOBS), data = ShrubCCH_Data, notch = TRUE,varwidth = TRUE,
+     col = "white", border = "black", horizontal = TRUE, las = 2,
+     pch = 1, ylab = "Basal Area Increment", xlab = "")
+
+
+#Plot Height ad a function of Stem Diameter
+plot(StemHeight ~ StemDiam, data = ShrubCCH_SALIX,
+     col = "black", pch = 1, ylab = "Basal Area Increment", xlab = "Ring Age (years)")
+
+ggplot(data = ShrubCCH_Data, 
+       aes(x = StemDiam,
+           y = StemHeight,
+           colour=Species)) + 
+  geom_point(size = 2) +
+  geom_smooth(method= "lm") +
+  xlab("Diameter (mm") + ylab("Height (cm)")
 
 #Plot BAI as a function of Age
 ShrubCCH_BENA <- subset(ShrubCCH_Data, Species == "BENA", select = ShrubID : Age)
                              
-ShrubCCH_SALIX <- subset(ShrubCCH_Data, grepl("SA", names(ShrubCCH_Data), value = TRUE), select = ShrubID : Age)
+ShrubCCH_SALIX <- subset(ShrubCCH_Data, grepl("^SA", ShrubCCH_Data$Species), select = ShrubID : Age)
 
-ShrubCCH_SALIX = data.frame(ShrubCCH_Data %>% dplyr:: select (grep("SA", names(ShrubCCH_Data))), check.names = FALSE)
+par(mfrow=c(1,2))
 
 plot(BAI ~ Age, data = ShrubCCH_BENA,
-     col = "black", pch = 1, ylab = "Basal Area Increment", xlab = "Ring Age (years)")
+     col = "black", pch = 1, ylab = "Basal Area Increment", xlab = "Ring Age (years)", ylim=c(0, 83), main = "Betula")
+
+plot(BAI ~ Age, data = ShrubCCH_SALIX,
+     col = "blue", pch = 1, ylab = "Basal Area Increment", xlab = "Ring Age (years)", ylim=c(0, 83), main = "Salix")
+
+par(mfrow=c(1,1))
+colors = c("Black", "Red", "Red", "Red")
+
+plot(BAI ~ Age, data = ShrubCCH_Data,
+     col = colors[ unclass(as.factor(ShrubCCH_Data$Species))], pch = 1, ylab = "Basal Area Increment", xlab = "Ring Age (years)")
+
+legend("topright", inset = 0.02, legend = c("Betula", "Salix"), col = c("black", "red"), pch = 1)
+
 
 # Develop mixed effects models 
 
