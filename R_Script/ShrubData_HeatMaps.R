576d2e17bf0e3d141e121212956e91abb8d791b4
Peter <pafrank89@gmail.comgit config --global user.email pafrank89@gmail.com>
2020-02-05 10:56:18 +0100

    Update

--- R_Script/ShrubData_HeatMaps.R
+++ R_Script/ShrubData_HeatMaps.R
@@ -1,38 +1,36 @@
+# DEVELOP HEAT MAPS FOR OPTIMAL BETULA MODEL ####
+CH1H2_model_b = lme(resid ~ iem.summ.temp * iem.summ.rain.10 + iem.summ.temp + iem.summ.rain.10 +
+                HareIndex + MooseDensity, 
+                data = sd_bena_cch, random = ~ 1|Section/ShrubID,
+                method = "REML")
 
-sd_final_cch$iem.summ.rain.10 = sd_final_cch$iem.summ.rain/10
+summary(CH1H2_model_b)
 
-#My final Model
-CH2_model = lme(resid ~ iem.summ.temp + iem.summ.rain + iem.summ.temp * iem.summ.rain +
-                HareIndex, 
-                data = sd_final_cch, random = ~ 1|Section/ShrubID,
-                method = "ML")
+HI = mean(sd_bena_cch$HareIndex)
+MD = mean(sd_bena_cch$MooseDensity)
+range(sd_bena_cch$iem.summ.temp)
+range(sd_bena_cch$iem.summ.rain.10)
 
-HI<-mean(sd_final_cch$HareIndex)
+# Predict
+MyData_b<-expand.grid(iem.summ.temp = seq(6, 18, length = 100),   #min and max of temp
+                    iem.summ.rain.10 = seq(3, 48, length = 100), #min and max of pre
+                    HareIndex = HI, MooseDensity = MD)
 
-range(sd_final_cch$iem.summ.temp)
-range(sd_final_cch$iem.summ.rain.10)
 
-# 4. Predict
-MyData<-expand.grid(iem.summ.temp = seq(6, 18, length = 100),   #min and max of temp
-                    iem.summ.rain = seq(30, 480, length = 100), #min and max of pre
-                    HareIndex = HI
-                    
-)
-MyData$Pred <- predict(CH2_model, MyData, level = 0) #Predicts the values based on model
+MyData_b$Pred <- predict(CH1H2_model_b, MyData_b, level = 0) #Predicts the values based on model
 
 # Calculate SEs
-Designmat <- model.matrix(formula(CH2_model)[-2], MyData) ## [-2] drops response from formula
-predvar <- diag(Designmat %*% vcov(CH2_model) %*% t(Designmat)) 
-MyData$SE <- sqrt(predvar)
-MyData$SEup<-MyData$SE+MyData$Pred
-MyData$SEdown<-MyData$Pred-MyData$SE
+Designmat <- model.matrix(formula(CH1H2_model_b)[-2], MyData_b) ## [-2] drops response from formula
+predvar <- diag(Designmat %*% vcov(CH1H2_model_b) %*% t(Designmat)) 
+MyData_b$SE <- sqrt(predvar)
+MyData_b$SEup<-MyData_b$SE+MyData_b$Pred
+MyData_b$SEdown<-MyData_b$Pred-MyData_b$SE
 
-
-# 5. Plot
-col.l <- colorRampPalette(c( 'white','darkgreen'))
-z<-c(0:10)
-p<-contourplot(Pred ~ iem.summ.temp + iem.summ.rain,
-               data=MyData,
+# Plot
+col.l = colorRampPalette(c( 'white','darkgreen'))
+z = c(0:10)
+pb = contourplot(Pred ~ iem.summ.temp + iem.summ.rain.10,
+               data=MyData_b,
                xlab="mean summer temperature",
                ylab="mean summer precip",
                pretty=TRUE,
@@ -42,58 +40,214 @@ p<-contourplot(Pred ~ iem.summ.temp + iem.summ.rain,
                labels=list(cex=1),
                col.regions=col.l,
                region=TRUE,
-               main=list("Shrub radial growth (Age standardized BAI mm2)", cex=1)
-)
+               main=list("Betula radial growth (residual values)", cex=1))
 
-p
+pb
 
-#Plot Standard error lines 
-p<-p+contourplot(SEup ~ iem.summ.temp + iem.summ.rain, 
-                 data=MyData,
+# Plot Standard error lines 
+pb = pb + contourplot(SEup ~ iem.summ.temp + iem.summ.rain.10, 
+                 data = MyData_b,
                  cuts=10,
-                 at = c(74), #change these when you see the plot
+                 at = c(0.2), #change these when you see the plot
+                 pretty=TRUE,
+                 lty=2,
+                 zlim = range(z, finite = TRUE),
+                 lwd=0.5,
+                 labels = list(cex=0),
+                 region=FALSE,
+                 main = list("Betula radial growth (residual values)", cex = 1))
+
+pb = pb + contourplot(SEdown ~ iem.summ.temp + iem.summ.rain.10, 
+                 data=MyData_b,
+                 cuts=10,
+                 at = c(0.2), #change these when you see the plot
                  pretty=TRUE,
                  lty=2,
                  zlim = range(z, finite = TRUE),
                  lwd=0.5,
                  labels=list(cex=0),
                  region=FALSE,
-                 main = list("Shrub radial growth (Age standardized BAI (mm2))", cex = 1)
-)
-p<-p+contourplot(SEdown ~ iem.summ.temp + iem.summ.rain, 
-                 data=MyData,
+                 main = list("Betula radial growth (residual values)", cex = 1, font=1))
+
+pb
+
+# DEVELOP HEAT MAPS FOR OPTIMAL BETULA MODEL WITH INTERACTIONS####
+CH_I_model_b = lme(resid ~ iem.summ.temp * iem.summ.rain.10 + iem.summ.temp + iem.summ.rain.10 +
+                    HareIndex + MooseDensity +
+                    iem.summ.temp:HareIndex, 
+                    data = sd_bena_cch, random = ~ 1|Section/ShrubID,
+                    method = "REML")
+
+summary(CH_I_model_b)
+
+HI = mean(sd_bena_cch$HareIndex)
+MD = mean(sd_bena_cch$MooseDensity)
+range(sd_bena_cch$iem.summ.temp)
+range(sd_bena_cch$iem.summ.rain.10)
+
+# Predict
+MyData_b<-expand.grid(iem.summ.temp = seq(6, 18, length = 100),   #min and max of temp
+                      iem.summ.rain.10 = seq(3, 48, length = 100), #min and max of pre
+                      HareIndex = seq(1, 3, length = 100),
+                      MooseDensity = MD)
+
+
+MyData_b$Pred <- predict(CH_I_model_b, MyData_b, level = 0) #Predicts the values based on model
+
+# Calculate SEs
+Designmat <- model.matrix(formula(CH_I_model_b)[-2], MyData_b) ## [-2] drops response from formula
+predvar <- diag(Designmat %*% vcov(CH_I_model_b) %*% t(Designmat)) 
+MyData_b$SE <- sqrt(predvar)
+MyData_b$SEup<-MyData_b$SE+MyData_b$Pred
+MyData_b$SEdown<-MyData_b$Pred-MyData_b$SE
+
+# Plot
+col.l = colorRampPalette(c( 'white','darkgreen'))
+z = c(0:10)
+pb = contourplot(Pred ~ iem.summ.temp + HareIndex,
+                 data=MyData_b,
+                 xlab="mean summer temperature",
+                 ylab="Hare Index",
+                 pretty=TRUE,
+                 lty=1,
+                 zlim=range(z, finite=TRUE),
+                 lwd=0.5,
+                 labels=list(cex=1),
+                 col.regions=col.l,
+                 region=TRUE,
+                 main=list("Betula radial growth (residual values)", cex=1))
+
+pb
+
+# Plot Standard error lines 
+pb = pb + contourplot(SEup ~ iem.summ.temp + iem.summ.rain.10, 
+                      data = MyData_b,
+                      cuts=10,
+                      at = c(0.2), #change these when you see the plot
+                      pretty=TRUE,
+                      lty=2,
+                      zlim = range(z, finite = TRUE),
+                      lwd=0.5,
+                      labels = list(cex=0),
+                      region=FALSE,
+                      main = list("Betula radial growth (residual values)", cex = 1))
+
+pb = pb + contourplot(SEdown ~ iem.summ.temp + iem.summ.rain.10, 
+                      data=MyData_b,
+                      cuts=10,
+                      at = c(0.2), #change these when you see the plot
+                      pretty=TRUE,
+                      lty=2,
+                      zlim = range(z, finite = TRUE),
+                      lwd=0.5,
+                      labels=list(cex=0),
+                      region=FALSE,
+                      main = list("Betula radial growth (residual values)", cex = 1, font=1))
+
+pb
+
+# DEVELOP HEAT MAPS FOR OPTIMAL SALIX MODEL ####
+
+CH1_model_s = lme(resid ~ iem.summ.temp + iem.summ.rain.10 + iem.summ.temp * iem.summ.rain.10 +
+                  MooseDensity, 
+                  data = sd_salix_cch, random = ~ 1|Section/ShrubID,
+                  method = "REML")
+
+summary(CH1_model_s)
+
+MDs = mean(sd_salix_cch$MooseDensity)
+range(sd_salix_cch$iem.summ.temp)
+range(sd_salix_cch$iem.summ.rain.10)
+
+# Predict
+MyData_s<-expand.grid(iem.summ.temp = seq(6, 18, length = 100),   #min and max of temp
+                    iem.summ.rain.10 = seq(3, 48, length = 100), #min and max of pre
+                    MooseDensity = MDs)
+
+
+MyData_s$Pred <- predict(CH1_model_s, MyData_s, level = 0) #Predicts the values based on model
+
+# Calculate SEs
+Designmat <- model.matrix(formula(CH1_model_s)[-2], MyData_s) ## [-2] drops response from formula
+predvar <- diag(Designmat %*% vcov(CH1_model_s) %*% t(Designmat)) 
+MyData_s$SE <- sqrt(predvar)
+MyData_s$SEup<-MyData_s$SE+MyData_s$Pred
+MyData_s$SEdown<-MyData_s$Pred-MyData_s$SE
+
+
+# Plot
+col.l = colorRampPalette(c( 'white','darkgreen'))
+z = c(0:10)
+ps = contourplot(Pred ~ iem.summ.temp + iem.summ.rain.10,
+                data=MyData_s,
+                xlab="mean summer temperature",
+                ylab="mean summer precip",
+                pretty=TRUE,
+                lty=1,
+                zlim=range(z, finite=TRUE),
+                lwd=0.5,
+                labels=list(cex=1),
+                col.regions=col.l,
+                region=TRUE,
+                main=list("Salix radial growth (residual values)", cex=1))
+
+ps
+
+# Plot Standard error lines 
+ps = ps + contourplot(SEup ~ iem.summ.temp + iem.summ.rain.10, 
+                 data = MyData_s,
                  cuts=10,
-                 at = c(74), #change these when you see the plot
+                 at = c(0.2), #change these when you see the plot
+                 pretty=TRUE,
+                 lty=2,
+                 zlim = range(z, finite = TRUE),
+                 lwd=0.5,
+                 labels = list(cex=0),
+                 region=FALSE,
+                 main = list("Salix radial growth (residual values)", cex = 1))
+
+ps = ps + contourplot(SEdown ~ iem.summ.temp + iem.summ.rain.10, 
+                 data=MyData_s,
+                 cuts=10,
+                 at = c(0.2), #change these when you see the plot
                  pretty=TRUE,
                  lty=2,
                  zlim = range(z, finite = TRUE),
                  lwd=0.5,
                  labels=list(cex=0),
                  region=FALSE,
-                 main = list("Shrub radial growth (Age standardized BAI (mm2))", cex = 1
-                             , font=1)
-)
+                 main = list("Salix radial growth (residual values)", cex = 1, font=1))
+
+ps
 
 
+# PLOT SAMPLES POINTS ON CONTURE GRAPHS ####
 
-p
+growth = tapply(sd_bena_cch$resid,list(sd_bena_cch$yearsitecombo, mean)) 
 
 
-
-# plot color by growth & size by sample size:
-
-growth<-tapply(denfPin$res,list(denfPin$YearCal),mean)     
 names <- rownames(growth)
+
 rownames(growth) <- NULL
+
 growth <- cbind(names,growth)
+
 colnames(growth)[colnames(growth)=="names"] <- "YearCal"
-deer<-tapply(denfPin$RZdeertotalkm2,list(denfPin$YearCal),mean)     
+
+deer<-tapply(denfPin$RZdeertotalkm2,list(denfPin$YearCal),mean)  
+
 names <- rownames(deer)
+
 rownames(deer) <- NULL
+
 deer <- cbind(names,deer)
+
 colnames(deer)[colnames(deer)=="names"] <- "YearCal"
-temp<-tapply(denfPin$mean.temp,list(denfPin$YearCal),mean)     
+
+temp<-tapply(denfPin$mean.temp,list(denfPin$YearCal),mean)   
+
 names <- rownames(temp)
+
 rownames(temp) <- NULL
 temp <- cbind(names,temp)
 colnames(temp)[colnames(temp)=="names"] <- "YearCal"
